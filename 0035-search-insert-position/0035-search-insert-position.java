ba4a2c285b8858260be597b3c8f8fa1acbf09489
aus10-tech <aus10.watts@gmail.com>
2025-05-22 15:22:22 -0400

    Time: 0 ms (100%), Space: 43 MB (51.69%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int targetIndex =0;
+        for(int i = 0; i < nums.length; i ++){
+            if(nums[i]== target){
+                targetIndex = i;
+                break;
+            }else if(nums[i]>target){
+                targetIndex = i;
+                break;
+            }else if(nums[nums.length-1]<target){
+                targetIndex = nums.length;
+            }
+        }return targetIndex;
+    }
+}
